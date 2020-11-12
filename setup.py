63e9cf8a890b8181f5f75072f8a9fe4d14c387c8
deadc0de6 <info@deadc0de.ch>
2020-11-12 15:51:20 +0100

    add more python version support

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           ],
 
