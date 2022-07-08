8482a77c237f6e4901d76626c7e2ee0d0a12c3ba
deadc0de6 <info@deadc0de.ch>
2022-07-08 13:59:03 +0200

    move everything to setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,7 @@ from os import path
 
 readme = 'README.md'
 here = path.abspath(path.dirname(__file__))
-
-try:
-    from pypandoc import convert_file
-    read_readme = lambda f: convert_file(f, 'rst')
-except ImportError:
-    print('\n[WARNING] pypandoc not found, could not convert \"{}\"\n'.format(readme))
-    read_readme = lambda f: open(f, 'r').read()
+read_readme = lambda f: open(f, 'r').read()
 
 VERSION = VERSION
 REQUIRES_PYTHON = '>=3.4'
@@ -22,8 +16,13 @@ setup(
 
     description='i3wm efficient real estate',
     long_description=read_readme(readme),
+    long_description_content_type='text/markdown'
+    license_files = ('LICENSE',),
     url='https://github.com/deadc0de6/i3altlayout',
     download_url = 'https://github.com/deadc0de6/i3altlayout/archive/v'+VERSION+'.tar.gz',
+    options={"bdist_wheel": {"python_tag": "py3"}},
+    # include anything from MANIFEST.in
+    include_package_data=True,
 
     author='deadc0de6',
     author_email='deadc0de6@foo.bar',
